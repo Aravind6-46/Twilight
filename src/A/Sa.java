ef6ea1320c6a92848dc3ce91e13632f45bbd23f2
Aravind6-46 <155824374+Aravind6-46@users.noreply.github.com>
2024-01-09 22:18:44 +0530

    2nd Commit.....

---
+++ src/A/Sa.java
@@ -0,0 +1,33 @@
+package A;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Sa {
+
+	public static void main(String[] args) throws InterruptedException {
+		
+WebDriver driver = new ChromeDriver();
+		
+		driver.get("https://www.saucedemo.com/v1/");
+		
+		driver.manage().window().maximize();
+		
+		driver.findElement(By.xpath("//input[@id=\"user-name\"]")).sendKeys("standard_user");
+		
+		driver.findElement(By.xpath("//input[@id=\"password\"]")).sendKeys("secret_sauce");
+		
+		driver.findElement(By.xpath("//input[@id=\"login-button\"]")).click();
+		
+		Thread.sleep(2000);
+
+		driver.navigate().back();
+		
+		Thread.sleep(2000);
+		
+		driver.close();
+
+	}
+
+}
